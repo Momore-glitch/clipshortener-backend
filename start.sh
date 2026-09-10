3012ac4ec401799a93913872b0ec9f257709c551
Momore-glitch <197477492+Momore-glitch@users.noreply.github.com>
2026-09-10 06:52:13 +0100

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-echo "[startup] Starting ClipShortener API..."
-echo "[startup] Acquisition handled by external acquisition service."
-
-exec python3 -m uvicorn app:app \
-  --host 0.0.0.0 \
-  --port "${PORT:-10000}"
